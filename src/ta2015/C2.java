54efdcaa38800f8511455c7cfe40fc420714b56d
hequet <hequet@b12p12.fil.univ-lille1.fr>
2015-09-18 15:34:58 +0200

    Create C2

---
+++ src/ta2015/C2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ta2015;
+
+/**
+ * @author hequet
+ *
+ */
+public class C2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
